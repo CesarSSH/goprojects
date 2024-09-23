47438cffbc77e0535f7ad5422d0c6e4475ca2f78
cesar3435ru <cesssuperbro345@gmail.com>
2024-09-22 19:50:23 -0600

    first hello world in GO

---
+++ helloworld.go
@@ -0,0 +1,9 @@
+//This is the GO'S kernel
+package main
+
+//It's a package to format basic strings, values, inputs and outputs. Basically, it  allows to write from the terminal
+import "fmt"
+
+func main() {
+	fmt.Println("Hello wordl...")
+}
