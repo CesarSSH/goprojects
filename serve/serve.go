f87e5db70ac612756aa59bc5b572581af5309ba0
cesarssh.dev@gmail.com <cesarssh.dev@gmail.com>
2024-10-06 20:45:06 -0600

    Easy server config release

--- serve/serve.go
+++ serve/serve.go
@@ -10,7 +10,29 @@ type Server struct {
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from the server"))
+	// w.Write([]byte("Hello from the server"))
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			w.Write([]byte("Index page"))
+			return
+		case "/users":
+			w.Write([]byte("Users page"))
+			return
+		case "/admins":
+			w.Write([]byte("Admins page"))
+			return
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Page Not Found"))
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Invalid method"))
+
+	}
 }
 
 func StartServer() {
